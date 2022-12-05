7a99cd70c0d33ec156d5dbf069a2143bf9d34b5b
MultiP-Dev <mariustichieru@gmail.com>
2022-12-05 22:09:27 +0100

    [backend] Add controllers
    Add controllers for Products, Categories and Ratings

---
+++ backend/WishlistAPI/Controllers/RatingsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WishlistAPI.DataAccess;
+using WishlistAPI.Models;
+
+namespace WishlistAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerBase
+    {
+        private readonly WishlistDBContext _context;
+
+        public RatingsController(WishlistDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ratings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Rating>>> GetRatings()
+        {
+            return await _context.Ratings.ToListAsync();
+        }
+
+        // GET: api/Ratings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Rating>> GetRating(int id)
+        {
+            var rating = await _context.Ratings.FindAsync(id);
+
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            return rating;
+        }
+
+        // PUT: api/Ratings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRating(int id, Rating rating)
+        {
+            if (id != rating.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(rating).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RatingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ratings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Rating>> PostRating(Rating rating)
+        {
+            _context.Ratings.Add(rating);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRating", new { id = rating.Id }, rating);
+        }
+
+        // DELETE: api/Ratings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRating(int id)
+        {
+            var rating = await _context.Ratings.FindAsync(id);
+            if (rating == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ratings.Remove(rating);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RatingExists(int id)
+        {
+            return _context.Ratings.Any(e => e.Id == id);
+        }
+    }
+}
