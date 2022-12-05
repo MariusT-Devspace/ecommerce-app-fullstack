76ae6530002afa3958ce16aca3e9e6b5e60c6e4b
MultiP-Dev <mariustichieru@gmail.com>
2022-12-05 22:01:43 +0100

    [backend] Add DbContext and migrations

---
+++ backend/WishlistAPI/DataAccess/WishlistDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WishlistAPI.Models;
+
+namespace WishlistAPI.DataAccess
+{
+    public class WishlistDBContext: DbContext
+    {
+        public WishlistDBContext(DbContextOptions<WishlistDBContext> options): base(options) { }
+
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Rating>? Ratings { get; set; }
+    }
+}
--- backend/WishlistAPI/Program.cs
+++ backend/WishlistAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WishlistAPI.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Connection to PostgreSQL
+const string CONNECTIONNAME = "WishlistDB";
+var connectionString = builder.Configuration.GetConnectionString(CONNECTIONNAME);
+
+// Add DbContext
+builder.Services.AddDbContext<WishlistDBContext>(options => options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
