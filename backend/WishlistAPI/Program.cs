0b7cc1de36f94e3ca79c8dcb99c507e4b94f2674
MultiP-Dev <mariustichieru@gmail.com>
2022-12-08 00:53:29 +0100

    [backend] Configure program for Azure deployment

--- backend/WishlistAPI/Program.cs
+++ backend/WishlistAPI/Program.cs
@@ -1,21 +1,45 @@
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using WishlistAPI.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+var keyVaultEndpoint = new Uri("https://wishlistapivault.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Connection to PostgreSQL
-const string CONNECTIONNAME = "WishlistDB";
-var connectionString = builder.Configuration.GetConnectionString(CONNECTIONNAME);
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+};
+
+
+var client = new SecretClient(keyVaultEndpoint, new DefaultAzureCredential(), options);
+
+KeyVaultSecret secret = client.GetSecret("WishlistAPIConnectionString");
+
+string secretValue = secret.Value;
+
 
 // Add DbContext
-builder.Services.AddDbContext<WishlistDBContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<WishlistDBContext>(options => options.UseSqlServer(secretValue));
 
 // CORS Configuration
 builder.Services.AddCors(options =>
