748843e0da27a36108ffa13c3347d9827818b870
MultiP-Dev <mariustichieru@gmail.com>
2022-12-08 19:31:41 +0100

    [backend] Create Register model

--- backend/WishlistAPI/Models/Register.cs
+++ backend/WishlistAPI/Models/Register.cs
@@ -1,6 +1,22 @@
-﻿namespace WishlistAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishlistAPI.Models
 {
     public class Register
     {
+        [Required, StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required, StringLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
