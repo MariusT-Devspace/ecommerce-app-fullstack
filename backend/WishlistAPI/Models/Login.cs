f5d148b2abe61a4fd4e2a6cc7227ac9ae2bf6670
MultiP-Dev <mariustichieru@gmail.com>
2022-12-11 10:49:41 +0100

    [backend] Create Login model

---
+++ backend/WishlistAPI/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishlistAPI.Models
+{
+    public class Login
+    {
+        [Required]
+        public string LoginInput { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
