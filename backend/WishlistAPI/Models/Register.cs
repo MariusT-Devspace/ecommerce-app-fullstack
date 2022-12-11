f3d713dfd27c7e96b3bc55c905922a90f79ca96a
MultiP-Dev <mariustichieru@gmail.com>
2022-12-11 19:25:00 +0100

    [backend] Add Email and Username validation

--- backend/WishlistAPI/Models/Register.cs
+++ backend/WishlistAPI/Models/Register.cs
@@ -11,9 +11,10 @@ namespace WishlistAPI.Models
         public string LastName { get; set; } = string.Empty;
 
         [Required, EmailAddress]
+        [StringLength(100, ErrorMessage = "The email address must be 100 characters long or less")]
         public string Email { get; set; } = string.Empty;
 
-        [Required, StringLength(50)]
+        [Required, StringLength(20, MinimumLength = 3, ErrorMessage = "The user name must be between {2} and {1} characters long")]
         public string Username { get; set; } = string.Empty;
 
         [Required]
