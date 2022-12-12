0036157c9fe0815ea8e0353250d793be3c68855b
MultiP-Dev <mariustichieru@gmail.com>
2022-12-12 23:59:38 +0100

    [backend] Fix UsersController id data type
    Change request id data type from int to string

--- backend/WishlistAPI/Controllers/UsersController.cs
+++ backend/WishlistAPI/Controllers/UsersController.cs
@@ -40,7 +40,7 @@ namespace WishlistAPI.Controllers
         // PUT: api/Users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
+        public async Task<IActionResult> PutUser(string id, User user)
         {
             if (id != user.Id)
             {
@@ -95,7 +95,7 @@ namespace WishlistAPI.Controllers
             return NoContent();
         }
 
-        private bool UserExists(int id)
+        private bool UserExists(string id)
         {
             return _context.Users.Any(e => e.Id == id);
         }
