f3961a2bb77085499880ba030622d26e3f76baa0
MultiP-Dev <mariustichieru@gmail.com>
2022-12-12 20:43:21 +0100

    [backend] Implement password hasher
    Implement the PasswordHasher class to hash and verify passwords.

---
+++ backend/WishlistAPI/Identity/PasswordHasher.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using PasswordVerificationResult = Microsoft.AspNetCore.Identity.PasswordVerificationResult;
+
+namespace WishlistAPI.Identity
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(IUser user, string password)
+        {
+            var passwordHasher = new PasswordHasher<IUser>();
+            string hashedPassword = passwordHasher.HashPassword(user, password);
+            return hashedPassword;
+        }
+
+        public bool VerifyPassword(IUser user, string password, string hashedPassword)
+        {
+            var passwordHasher = new PasswordHasher<IUser>();
+            var result = passwordHasher.VerifyHashedPassword(user, hashedPassword, password);
+
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
--- backend/WishlistAPI/Models/DataModels/User.cs
+++ backend/WishlistAPI/Models/DataModels/User.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace WishlistAPI.Models.DataModels
 {
-    public class User: BaseEntity
+    public class User: IUser
     {
         public enum UserRole
         {
             Administrator,
             User
         }
+        string IUser<string>.Id { get; } = string.Empty;
+
+        [Required, StringLength(50)]
+        string IUser<string>.UserName { get; set; } = string.Empty;
 
         [Required, StringLength(50)]
         public string FirstName { get; set; } = string.Empty;
@@ -19,16 +24,18 @@ namespace WishlistAPI.Models.DataModels
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
 
-        [Required, StringLength(50)]
-        public string Username { get; set; } = string.Empty;
-
         [Required]
         public string PasswordHash { get; set; } = string.Empty;
 
         [Required]
         public UserRole? Role { get; set; } = UserRole.User;
 
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+        public DateTime? DeletedOn { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
         public virtual ICollection<WishlistItem> Wishlist { get; set; } = new List<WishlistItem>();
-        
+
     }
 }
