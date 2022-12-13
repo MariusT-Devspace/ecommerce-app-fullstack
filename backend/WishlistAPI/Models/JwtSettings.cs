528458f686845682dba0c5f10e2da1db80ff4a88
MultiP-Dev <mariustichieru@gmail.com>
2022-12-13 16:58:22 +0100

    [backend] Add JwtSettings model

---
+++ backend/WishlistAPI/Models/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace WishlistAPI.Models
+{
+    public class JwtSettings
+    {
+        public bool ValidateIssuerSigningKey { get; set; }
+        public string IssuerSigningKey { get; set; } = string.Empty;
+
+        public bool ValidateIssuer { get; set; }
+        public string ValidIssuer { get; set; } = string.Empty;
+
+        public bool ValidateAudience { get; set; }
+        public string ValidAudience { get; set; } = string.Empty;
+
+        public bool RequireExpirationTime { get; set; }
+        public bool ValidateLifeTime { get; set; }
+    }
+}
