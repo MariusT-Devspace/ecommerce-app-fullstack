0fd5055dcf6ab47460eae79abc6cf60af54dd5bf
MultiP-Dev <mariustichieru@gmail.com>
2022-12-13 22:25:36 +0100

    [backend] Add UserToken model
    This is the model for the token api respone

---
+++ backend/WishlistAPI/Models/UserToken.cs
@@ -0,0 +1,17 @@
+﻿using static WishlistAPI.Models.DataModels.User;
+
+namespace WishlistAPI.Models
+{
+    public class UserToken
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+        public string Token { get; set; } = string.Empty;
+        public UserRole? Role { get; set; }
+        public string WelcomeMessage { get; set; } = string.Empty;
+    }
+}
