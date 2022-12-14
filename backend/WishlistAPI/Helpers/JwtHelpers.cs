8e22fe08f39c11f06d1dbed814d6b8d88e4e51da
MultiP-Dev <mariustichieru@gmail.com>
2022-12-14 08:50:49 +0100

    [backend] Add JwtHelpers for token creation
    This class contains extension methods, which are called on a UserToken instance and use its data to create the token key, which is then returned and assigned to the Token field of the same UserToken instance.

---
+++ backend/WishlistAPI/Helpers/JwtHelpers.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using WishlistAPI.Models;
+using WishlistAPI.Models.DataModels;
+
+namespace WishlistAPI.Helpers
+{
+    public static class JwtHelpers
+    {
+        public static IEnumerable<Claim> GetClaims(UserToken userToken)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim("UserId", userToken.UserId),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Name, userToken.UserName),
+                new Claim(ClaimTypes.Email, userToken.Email),
+                // Expires in 1 day
+                new Claim(ClaimTypes.Expiration, userToken.Expiration.ToString("MMM ddd dd yyyy HH:mm:ss tt"))
+            };
+
+            if (userToken.Role == User.UserRole.Administrator)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, User.UserRole.Administrator.ToString()));
+            }
+            else
+            {
+                claims.Add(new Claim(ClaimTypes.Role, User.UserRole.User.ToString()));
+            }
+
+            return claims;
+        }
+
+        public static string GetTokenKey(this UserToken userToken, JwtSettings jwtSettings)
+        {
+            try
+            {
+                // Obtain secret key
+                var key = System.Text.Encoding.ASCII.GetBytes(jwtSettings.IssuerSigningKey);
+
+                // Generate JWT
+                var jwtToken = new JwtSecurityToken(
+                    issuer: jwtSettings.ValidIssuer,
+                    audience: jwtSettings.ValidAudience,
+                    claims: GetClaims(userToken),
+                    notBefore: new DateTimeOffset(DateTime.Now).DateTime,
+                    expires: new DateTimeOffset(userToken.Expiration).DateTime,
+                    signingCredentials: new SigningCredentials(
+                        new SymmetricSecurityKey(key),
+                        SecurityAlgorithms.HmacSha256
+                        )
+                    );
+
+                var token = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+                return token;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error generating the JWT", ex);
+            }
+        }
+    }
+}
