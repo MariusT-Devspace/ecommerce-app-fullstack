0e1dd3c1a5dd259dfe33c57c0f6a81844b4ed404
MultiP-Dev <mariustichieru@gmail.com>
2022-12-14 01:38:53 +0100

    [backend] Make PasswordHasher static

--- backend/WishlistAPI/Identity/PasswordHasher.cs
+++ backend/WishlistAPI/Identity/PasswordHasher.cs
@@ -4,16 +4,16 @@ using PasswordVerificationResult = Microsoft.AspNetCore.Identity.PasswordVerific
 
 namespace WishlistAPI.Identity
 {
-    public class PasswordHasher
+    public static class PasswordHasher
     {
-        public string HashPassword(IUser user, string password)
+        public static string HashPassword(IUser user, string password)
         {
             var passwordHasher = new PasswordHasher<IUser>();
             string hashedPassword = passwordHasher.HashPassword(user, password);
             return hashedPassword;
         }
 
-        public bool VerifyPassword(IUser user, string password, string hashedPassword)
+        public static bool VerifyPassword(IUser user, string password, string hashedPassword)
         {
             var passwordHasher = new PasswordHasher<IUser>();
             var result = passwordHasher.VerifyHashedPassword(user, hashedPassword, password);
