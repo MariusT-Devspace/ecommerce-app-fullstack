f0f7629457a9536e9f8298d296808450ed411a31
MultiP-Dev <mariustichieru@gmail.com>
2022-12-16 13:21:38 +0100

    [backend] Fix Price RegularExpression on Products entity

--- backend/WishlistAPI/Models/DataModels/Product.cs
+++ backend/WishlistAPI/Models/DataModels/Product.cs
@@ -12,7 +12,7 @@ namespace WishlistAPI.Models.DataModels
         public string Description { get; set; } = string.Empty;
 
         [Required]
-        [RegularExpression(@"^\d+\.\d{0,2}$")]
+        [RegularExpression(@"^\d+\.\d{2}$")]
         [Range(0, 9999999999999999.99)]
         public decimal Price { get; set; }
 
