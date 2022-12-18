41f80de38cb0fc1a0d7e1d761185e1a9c4680164
MultiP-Dev <mariustichieru@gmail.com>
2022-12-18 18:00:07 +0100

    [backend] Add mapping profile for Product

---
+++ backend/WishlistAPI/Models/MappingProfiles/ProductMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WishlistAPI.Models.DataModels;
+using WishlistAPI.Models.DTOs.Request;
+using WishlistAPI.Models.DTOs.Response;
+
+namespace WishlistAPI.Models.MappingProfiles
+{
+    public class ProductMappingProfile : Profile
+    {
+        public ProductMappingProfile() {
+            CreateMap<Product, ProductResponse>();
+            CreateMap<ProductRequest, Product>();
+        }
+    }
+}
