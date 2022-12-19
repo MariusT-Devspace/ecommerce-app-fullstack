3479b44875a4af9fa9aaa38391f83ba96a5fa3f3
MultiP-Dev <mariustichieru@gmail.com>
2022-12-19 01:03:25 +0100

    [backend] Add DTOs for Category entity

---
+++ backend/WishlistAPI/Models/DTOs/CategoryDTOs/Request/CategoryRequestPUT.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WishlistAPI.Models.DataModels;
+
+namespace WishlistAPI.Models.DTOs.CategoryDTOs.Request
+{
+    [MetadataType(typeof(Category))]
+    public class CategoryRequestPUT
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ backend/WishlistAPI/Models/DTOs/CategoryDTOs/Response/CategoryResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WishlistAPI.Models.DataModels;
+
+namespace WishlistAPI.Models.DTOs.CategoryDTOs.Response
+{
+    [MetadataType(typeof(Category))]
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
