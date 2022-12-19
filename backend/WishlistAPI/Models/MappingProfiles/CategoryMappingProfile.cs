44fc7ee684bb54d068207ef8e38ac62547c082b8
MultiP-Dev <mariustichieru@gmail.com>
2022-12-19 01:04:10 +0100

    [backend] Add Category mapping profile

---
+++ backend/WishlistAPI/Models/MappingProfiles/CategoryMappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using WishlistAPI.Models.DataModels;
+using WishlistAPI.Models.DTOs.CategoryDTOs.Request;
+using AutoMapper;
+using WishlistAPI.Models.DTOs.CategoryDTOs.Response;
+
+namespace WishlistAPI.Models.MappingProfiles
+{
+    public class CategoryMappingProfile: Profile
+    {
+        public CategoryMappingProfile() {
+            CreateMap<CategoryRequestPOST, Category>();
+            CreateMap<CategoryRequestPUT, Category>();
+            CreateMap<Category, CategoryResponse>();
+            CreateMap<Category, CategoryWithProductsResponse>();
+        }
+    }
+}
