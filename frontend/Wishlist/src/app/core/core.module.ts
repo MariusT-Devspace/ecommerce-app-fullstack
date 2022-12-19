2491033f5e9c2e1467449b623dfe996ff30a4cde
MultiP-Dev <mariustichieru@gmail.com>
2022-12-19 12:02:14 +0100

    [frontend] Add core module

---
+++ frontend/Wishlist/src/app/core/core.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatNavigationComponent } from './mat-navigation/mat-navigation.component';
+
+
+@NgModule({
+  declarations: [
+    MatNavigationComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class CoreModule { }
