aa55602a6cf544b844486f12547ddc52f1fc1bfa
MultiP-Dev <mariustichieru@gmail.com>
2022-12-20 19:54:00 +0100

    [frontend] Add product cards on the home page

--- frontend/Wishlist/src/app/home/home-page/home-page.component.ts
+++ frontend/Wishlist/src/app/home/home-page/home-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product/product.model';
+import { ProductsService } from '../../core/services/products.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.sass']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productsService: ProductsService){ }
+
+  ngOnInit(){
+    this.productsService.getProducts().subscribe({
+      next: (response: Product[]) => {this.products = response; console.log(`Product: ${response}`);},
+      error: (err: Error) => console.error("Could not retrieve product" + err.message),
+      complete: () => console.log("All products have been retrieved")
+    });
+  }
 }
