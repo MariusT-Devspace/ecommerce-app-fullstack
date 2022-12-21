6fef995b5c3ac771b3a01a5deef0e17033916a7e
MultiP-Dev <mariustichieru@gmail.com>
2022-12-21 03:13:31 +0100

    [frontend] Add sign up form component

---
+++ frontend/Wishlist/src/app/core/auth/sign-up-page/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-form',
+  templateUrl: './sign-up-form.component.html',
+  styleUrls: ['./sign-up-form.component.sass']
+})
+export class SignUpFormComponent {
+
+}
