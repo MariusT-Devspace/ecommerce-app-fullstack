3cfc26a2c620d0e2270f9cd98e0c76f2f3978a0f
Marius <mariustichieru@gmail.com>
2023-06-24 23:59:21 +0200

    [frontend] Move get categories in parent component
    Get categories in parent component so they can be passed to multiple child components.

--- frontend/Ecommerce/src/app/admin/admin-home-page/products-management-page/products-list/products-list.component.ts
+++ frontend/Ecommerce/src/app/admin/admin-home-page/products-management-page/products-list/products-list.component.ts
@@ -1,36 +1,30 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { map } from 'rxjs';
-import { CategoriesService } from 'src/app/core/services/categories.service';
-import { ICategory } from 'src/app/models/category.model';
-import { IProduct } from 'src/app/models/product.model';
 import { AddProductDialogComponent } from './add-product-dialog/add-product-dialog.component';
 import { ProductDetailDialogComponent } from './product-detail-dialog/product-detail-dialog.component';
+import { ICategory } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.sass']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent {
+  @Input() categories : ICategory[] = []
+
   breakpoint$ = this.breakpointObserver.observe(Breakpoints.Handset)
               .pipe(
                 map(result => result.matches ? 'handset' : 'desktop')
               );
 
-  categories: ICategory[] = [];
   _addProductDialogRef: MatDialogRef<AddProductDialogComponent> | undefined;
   _productDetailDialogRef: MatDialogRef<ProductDetailDialogComponent> | undefined;
   selectedProductId: number | undefined;
 
-  constructor(private categoriesService: CategoriesService, 
-    private breakpointObserver: BreakpointObserver, public dialog: MatDialog) { 
+  constructor(private breakpointObserver: BreakpointObserver, public dialog: MatDialog) { 
     }
-
-  ngOnInit(): void {
-    this.getCategories();
-  }
 
   openAddProductDialog(): void {
     let enterAnimationDuration = '300ms';
@@ -79,14 +73,6 @@ export class ProductsListComponent implements OnInit {
         productId: selectedProductId,
         categories: this.categories
       }
-    });
-  }
-
-  getCategories() {
-    this.categoriesService.getCategories().subscribe({
-      next: (response: ICategory[]) => this.categories = response,
-      error: (err: Error) => console.error("Error retrieving categories", err),
-      complete: () => console.log("Categories retrieved successfuly")
     });
   }
 
--- frontend/Ecommerce/src/app/admin/admin-home-page/products-management-page/products-management-page.component.ts
+++ frontend/Ecommerce/src/app/admin/admin-home-page/products-management-page/products-management-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/core/services/categories.service';
+import { ICategory } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-products-management-page',
   templateUrl: './products-management-page.component.html',
   styleUrls: ['./products-management-page.component.sass']
 })
-export class ProductsManagementPageComponent {
+export class ProductsManagementPageComponent implements OnInit{
+  categories : ICategory[] = []  
 
+  constructor (private categoriesService : CategoriesService) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoriesService.getCategories().subscribe({
+      next: (response: ICategory[]) => this.categories = response,
+      error: (err: Error) => console.error("Error retrieving categories", err),
+      complete: () => console.log("Categories retrieved successfuly")
+    });
+  }
 }
