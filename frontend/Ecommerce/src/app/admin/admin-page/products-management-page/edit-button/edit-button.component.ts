ffbb1babcb82894fb5073e0ac691330a7d0e8368
Marius <mariustichieru@gmail.com>
2023-07-03 23:58:49 +0200

    feat(frontend): Add edit button to categories list item
    Add edit button to categories list item on admin page

--- frontend/Ecommerce/src/app/admin/admin-page/products-management-page/edit-button/edit-button.component.ts
+++ frontend/Ecommerce/src/app/admin/admin-page/products-management-page/edit-button/edit-button.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./edit-button.component.sass']
 })
 export class EditButtonComponent {
-  EDIT_ICON = `<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M173 884h53l440-441-53-53-440 441v53Zm638-484L656 244l40.823-40.823Q718 182 749 182.5t52 21.5l52 53q21 21 20.5 51t-21.463 50.963L811 400ZM136.327 957q-15.889 0-26.108-10.714Q100 935.571 100 920.689V816.062q0-7.062 2.5-13.562Q105 796 110 791l505-505 155 155-504 505q-6 6-12.217 8.5Q247.565 957 241 957H136.327ZM640 417l-27-27 53 53-26-26Z"/></svg>`;
+  EDIT_ICON = `<svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20"><path d="M210.154-204h52.461l361.387-361.386-52.461-52.462-361.387 361.387V-204Zm549.152-404.77L615.309-753.537l45.538-45.538q19.231-19.23 45.461-19.23 26.23 0 45.46 19.23l53.461 53.461q18.231 18.23 17.846 45.845-.385 27.615-18.615 45.845l-45.154 45.154ZM184.309-140.001q-15.461 0-26.807-11.347-11.346-11.346-11.346-26.807v-90.767q0-7.231 2.615-13.769 2.615-6.539 8.231-12.154l413.923-413.924 143.998 143.998-413.924 413.923q-5.615 5.616-12.153 8.231-6.539 2.616-13.77 2.616h-90.767Zm413.77-451.308-26.538-26.539 52.461 52.462-25.923-25.923Z"/></svg>`;
 
   constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
     iconRegistry.addSvgIconLiteral('edit', sanitizer.bypassSecurityTrustHtml(this.EDIT_ICON));
