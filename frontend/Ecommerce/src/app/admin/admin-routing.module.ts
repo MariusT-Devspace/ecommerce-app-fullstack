7795ab21f230df4a07bf9b7dc754b21783b9573d
Marius <mariustichieru@gmail.com>
2023-11-19 17:14:53 +0100

    feat(frontend): Update angular to v17

--- frontend/Ecommerce/src/app/admin/admin-routing.module.ts
+++ frontend/Ecommerce/src/app/admin/admin-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
       },
       {
         path: 'users',
-        component:UsersPageComponent
+        component: UsersPageComponent
       },
       {
         path: '',
