1be40bb0273d931e811f6c103f2df2cea04bfbef
Marius <mariustichieru@gmail.com>
2024-01-02 18:15:55 +0100

    feat(frontend): Add shopping cart icon to "Add to cart" button

--- frontend/Ecommerce/src/app/home/home-page/product/product.component.ts
+++ frontend/Ecommerce/src/app/home/home-page/product/product.component.ts
@@ -2,6 +2,9 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs';
 import { IProduct } from 'src/app/models/product.model';
+import { IconButtonType } from 'src/app/shared/icon-button/icon-button-type.enum';
+import { IconButton } from 'src/app/shared/icon-button/icon-button.model';
+import { MaterialIcon } from 'src/app/shared/icon-button/material-icons.enum';
 
 @Component({
   selector: 'app-product',
@@ -25,6 +28,12 @@ export class ProductComponent implements OnInit{
               .pipe(
                 map(result => result.matches ? 'handset' : 'desktop')
               );
+
+  addToCartButton: IconButton = {
+    icon: { iconName: 'shopping-cart', svgIcon: MaterialIcon.SHOPPING_CART_FILL0_W300_GRAD0_SZ20 },
+    matButtonType: IconButtonType.MAT_BUTTON,
+    color: undefined
+  }
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
