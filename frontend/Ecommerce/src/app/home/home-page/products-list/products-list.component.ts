126224163b4f629fa25214938f9bb0a243684a5f
Marius <mariustichieru@gmail.com>
2024-01-24 15:03:46 +0100

    refactor(frontend): Switch to signal for product list

--- frontend/Ecommerce/src/app/home/home-page/products-list/products-list.component.ts
+++ frontend/Ecommerce/src/app/home/home-page/products-list/products-list.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, WritableSignal, signal } from '@angular/core';
 import { map } from 'rxjs/internal/operators/map';
 import { ProductsService } from 'src/app/core/services/products.service';
 import { IProduct } from 'src/app/models/product.model';
@@ -10,7 +10,7 @@ import { IProduct } from 'src/app/models/product.model';
     styleUrl: './products-list.component.sass',
 })
 export class ProductsListComponent implements OnInit{
-  products: IProduct[] = [];
+  products: WritableSignal<IProduct[]> = signal([]);
   
   breakpoint$ = this.breakpointObserver.observe(Breakpoints.XSmall)
   .pipe(
@@ -24,7 +24,7 @@ export class ProductsListComponent implements OnInit{
   
   ngOnInit(): void {
     this.productsService.getProducts().subscribe({
-      next: (response: IProduct[]) => {this.products = response; console.log(`Product: ${response}`);},
+      next: (response: IProduct[]) => {this.products.set(response); console.log(`Product: ${response}`);},
       error: (err: Error) => console.error("Could not retrieve product" + err.message),
       complete: () => console.log("All products have been retrieved")
     });
