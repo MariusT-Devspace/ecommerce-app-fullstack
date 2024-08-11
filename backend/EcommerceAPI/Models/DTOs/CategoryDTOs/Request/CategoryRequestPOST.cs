b9d283d8049e7ad16c098af268604ceac85bbf9b
Marius <mariustichieru@gmail.com>
2024-08-11 18:53:08 +0200

    feat(backend): Add category DTOs

---
+++ backend/EcommerceAPI/Models/DTOs/CategoryDTOs/Request/CategoryRequestPOST.cs
@@ -0,0 +1,11 @@
+﻿using EcommerceAPI.Models.DataModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceAPI.Models.DTOs.CategoryDTOs.Request
+{
+    [MetadataType(typeof(Category))]
+    public class CategoryRequestPOST
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ backend/EcommerceAPI/Models/DTOs/CategoryDTOs/Request/CategoryRequestPUT.cs
@@ -0,0 +1,12 @@
+﻿using EcommerceAPI.Models.DataModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceAPI.Models.DTOs.CategoryDTOs.Request
+{
+    [MetadataType(typeof(Category))]
+    public class CategoryRequestPUT
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
