6fc1404cd3486b8e3d44002b9934174c33be6385
Marius <mariustichieru@gmail.com>
2024-08-11 18:55:04 +0200

    feat(backend): Implement mapping between Category entity and DTOs

--- backend/EcommerceAPI/Models/MappingProfiles/CategoryMappingProfile.cs
+++ backend/EcommerceAPI/Models/MappingProfiles/CategoryMappingProfile.cs
@@ -1,15 +1,16 @@
 ﻿using EcommerceAPI.Models.DataModels;
-using EcommerceAPI.Models.DTOs.CategoryDTOs;
 using AutoMapper;
+using EcommerceAPI.Models.DTOs.CategoryDTOs;
+using EcommerceAPI.Models.DTOs.CategoryDTOs.Request;
 
 namespace EcommerceAPI.Models.MappingProfiles
 {
     public class CategoryMappingProfile: Profile
     {
         public CategoryMappingProfile() {
-            CreateMap<CategoryREST, Category>().ForMember(dest => dest.Id, act => act.Ignore())
-                                               .ForMember(dest => dest.CategoryId, act => act.MapFrom(src => src.Id));  
-            CreateMap<Category, CategoryREST>().ForMember(dest => dest.Id, act => act.MapFrom(src => src.CategoryId));
+            CreateMap<CategoryRequestPOST, Category>();
+            CreateMap<CategoryRequestPUT, Category>();
+            CreateMap<Category, CategoryResponse>();
         }
     }
 }
