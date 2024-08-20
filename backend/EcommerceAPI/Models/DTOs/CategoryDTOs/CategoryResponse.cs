e81dab2d097191185a6814ba030ce5d7db9c05f7
Marius <mariustichieru@gmail.com>
2024-08-21 00:09:23 +0200

    fix(backend): change remaining references to category id to int

--- backend/EcommerceAPI/Models/DTOs/CategoryDTOs/CategoryResponse.cs
+++ backend/EcommerceAPI/Models/DTOs/CategoryDTOs/CategoryResponse.cs
@@ -6,7 +6,7 @@ namespace EcommerceAPI.Models.DTOs.CategoryDTOs
     [MetadataType(typeof(Category))]
     public class CategoryResponse
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
     }
