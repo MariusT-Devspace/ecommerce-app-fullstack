42708762bcce2e44bd3653c30f976acb42e88024
Marius <mariustichieru@gmail.com>
2024-08-21 18:54:49 +0200

    feat(frontend): create ROUTES record constant containing slug and title properties

--- frontend/Ecommerce/src/app/constants.ts
+++ frontend/Ecommerce/src/app/constants.ts
@@ -4,3 +4,16 @@ export enum ThemeColor {
     WARN = 'warn',
     ERROR = 'error'
 }
+
+// Routes
+type Route = "products" | "users";
+
+interface RouteInfo {
+    slug: string;
+    title: string;
+}
+
+export const ROUTES: Record<Route, RouteInfo> = {
+    products: { slug: "products", title: "Products" },
+    users: { slug: "users", title: "Users" }
+}
